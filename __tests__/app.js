467e8d06d61fea11cc68b2f5faa7e5c500948c9d
Vitor Freitas <vitorldfreitas@gmail.com>
2020-01-31 01:05:09 -0300

    tests if the generator is being initialized

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,21 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
+const rimraf = require("rimraf");
 
-describe('generator-ts-monorepo:app', () => {
+describe("generator-ts-monorepo:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ name: "monorepo" });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  afterAll(() => {
+    rimraf.sync(path.join(__dirname, "tmp"));
+  });
+
+  it("should create the monorepo", () => {
+    assert.file(["../monorepo"]);
   });
 });
